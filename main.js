851d23754ba047942892864e9f7f16c9341e756e
dacoderboii <85032650+dacoderboii@users.noreply.github.com>
2021-06-12 08:45:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext('2d');
+car1_width = 120;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
+car1_y = 10;
+car2_width = 120;
+car2_height = 70;
+car2_image = "car2.png";
+car2_x = 10;
+car2_y = 10;
+background_image = "racing.png";
+function add() {
+    background_imgTag = new Image()
+    background_imgTag = uploadBackground;
+    background_imgTag.src = background_image;
+
+    
+}
